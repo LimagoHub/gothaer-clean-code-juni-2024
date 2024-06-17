519ebdc269334bbb11c8b06d39436f55802e4193
Jo Wagner <jowagner@limago.de>
2024-06-17 14:38:58 +0200

    Terminate Turn

--- NimSpielProjekt/src/de/gothaer/game/nimgame/NimGame.java
+++ NimSpielProjekt/src/de/gothaer/game/nimgame/NimGame.java
@@ -8,13 +8,13 @@ public class NimGame implements Game {
 
     private final Scanner scanner = new Scanner(System.in);
     private int stones;
-    private boolean gameover = false;
+    private int turn;
     public NimGame() {
         stones = 23;
     }
     @Override
     public void play() {
-        while( ! gameover) {
+        while( ! isGameover()) {
             playRound();
         }
     }
@@ -23,31 +23,45 @@ public class NimGame implements Game {
         computerTurn();
     }
     private void humanTurn() {
-        int turn;
+        if(isGameover()) return;
+
         while(true) {
             System.out.println("Es gibt " + stones + " Steine. Bitte nehmen Sie 1,2 oder 3!");
             turn = scanner.nextInt();
             if(turn >= 1 && turn <= 3) break;
             System.out.println("Ungueltiger Zug");
         }
-        stones -= turn;
+        terminateTurn( "Mensch");
     }
     private void computerTurn() {
+        if(isGameover()) return;
         final int [] turns = {3,1,1,2};
-        int turn;
-        if( stones < 1 ) {
-            System.out.println("Du Loser");
-            gameover = true;
-            return;
-        }
-        if( stones == 1 ) {
-            System.out.println("Du hast nur Glueck gehabt");
-            gameover = true;
-            return;
-        }
+
+
         turn = turns[stones % 4];
         System.out.println("Computer nimmt " + turn + " Steine.");
-        stones -= turn;
+        terminateTurn( "Computer");
 
+    }
+
+    private void terminateTurn(final String player) { // Integration
+        updateBoard();
+        printGameOverMessageIfGameIsOver(player);
+    }
+
+    private void printGameOverMessageIfGameIsOver(final String player) { // Operation
+        if(isGameover()) {
+            System.out.println(player + " hat verloren");
+        }
+    }
+
+
+    //---------------- Implementierungssumpf ------------------------
+    private void updateBoard() {
+        stones -= turn;
+    }
+
+    private boolean isGameover() {
+        return stones < 1;
     }
 }
